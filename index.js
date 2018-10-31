da4038078da28adfac4ee0bc657c60dc1e2b6047
Laszlo Uray <uray077@gmail.com>
2018-10-31 22:10:25 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,53 @@
 // alert('HEy HO');
 document.getElementById('date').innerHTML = Date();
+
+const itemsData = [{
+  date: '12/10/2018',
+  title: 'first',
+  status: 'done',
+},{
+  date: '12/12/2018',
+  title: 'second',
+  status: 'done',
+},{
+  date: '12/10/2018',
+  title: 'third',
+  status: 'done',
+}]
+
+
+const handleAddClick = (newItemData) => {
+  const updatedData =  [
+    ...itemsData,
+    newItemData,
+  ];
+
+  generateItemsHTML(updatedData);
+}
+
+const renderItem = ({
+  date,
+  title,
+  status
+}) => {
+  // createHTML
+  element.innerHTML = (
+    `<div>
+    <span>${title}</span>
+    <span>${dates}</span>
+    <span>${status}</span>
+    </div>`
+  )
+}
+
+const generateItemsHTML = () => {
+  let generatedHTML = '';
+  
+  // Assign HTML for all items to a variable
+  itemsData.forEach(item => {
+    generatedHTML += renderItem(item);
+  })
+
+  // Append generated HTML to List element
+  element.appendChild(generatedHTML);
+}
